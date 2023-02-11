252ce6f2039d7e376bd0481264e377188182977b
maz9-prog <58932463+maz9-prog@users.noreply.github.com>
2023-02-10 19:19:34 -0700

    Update DataUtilities_createNumberArray_2D.java

--- JFreeChartTestSuite/src/org/jfree/data/test/DataUtilities_createNumberArray_2D.java
+++ JFreeChartTestSuite/src/org/jfree/data/test/DataUtilities_createNumberArray_2D.java
@@ -2,12 +2,14 @@ package org.jfree.data.test;
 
 import static org.junit.Assert.*;
 
+import java.lang.IllegalArgumentException;
+
 import org.jfree.data.DataUtilities;
 
 
 import org.junit.*;
 
-public class DataUtilities_createNumberArray_2D{
+public class DataUtilitiesTest{
 	
 	@BeforeClass public static void setUpBeforeClass() throws Exception {
     }
@@ -18,11 +20,13 @@ public class DataUtilities_createNumberArray_2D{
 
     // For testing 1D Array
 Number[] Expectedarray = {1.0,2.0,3.0};
-double[] ParameterArray = {1.0,2.0,3.0};
 
+double[] ParameterArray = {1.0,2.0,3.0};
+double[] ExceptionParameterArray = null;
 	// For testing 2D Array
 Number [][] Expectedarray2D = {{1.0,2.0,3.0},{4.0,5.0,6.0}};
 double [][] ParameterArray2D = {{1.0,2.0,3.0},{4.0,5.0,6.0}};
+double [][] ExceptionParameterArray2D = null;
 
     // Test for 1D Array
     @Test
@@ -36,7 +40,27 @@ double [][] ParameterArray2D = {{1.0,2.0,3.0},{4.0,5.0,6.0}};
     	assertArrayEquals("An array of Number objects.",
     	Expectedarray2D, DataUtilities.createNumberArray2D(ParameterArray2D));
     }
+    @Test
+    public void testcreateNumberArrayOne() {
+    	Number[] Expectedarray = {1.0};
+    	double[] ParameterArray = {2.0};
+
+    	assertArrayEquals("An array of Number objects.",
+        Expectedarray, DataUtilities.createNumberArray(ParameterArray));
+    }
     
+    @Test(expected = IllegalArgumentException.class)
+    
+    public void createNumberArrayException() {
+    	DataUtilities.createNumberArray(ExceptionParameterArray); 
+    }
+    
+    @Test(expected = IllegalArgumentException.class)
+        
+    public void createNumberArrayException2D() {
+        DataUtilities.createNumberArray2D(ExceptionParameterArray2D);
+    }
+
   
     @After
     public void tearDown() throws Exception {
